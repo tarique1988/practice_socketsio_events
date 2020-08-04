e509118e125c08725b34a0e457cbd8bffc0ca28f
Tarique Mirza <sajmtam@gmail.com>
2020-08-04 19:26:17 +0530

    disable button when no input

--- public/js/main.js
+++ public/js/main.js
@@ -2,20 +2,33 @@ const socket = io();
 const submitBtn = document.getElementById("submitBtn");
 const messageInput = document.getElementById("message");
 const sendLocationBtn = document.getElementById("sendLocation");
+
+submitBtn.setAttribute("disabled", "disabled");
+
 socket.on("message", (message) => {
 	console.log(message);
 });
 
-messageInput.addEventListener("keypress", (e) => {
+messageInput.addEventListener("keyup", (e) => {
+	if (e.target.value.trim()) {
+		submitBtn.removeAttribute("disabled");
+	} else {
+		submitBtn.setAttribute("disabled", "disabled");
+		return;
+	}
 	if (e.key === "Enter") {
+		submitBtn.setAttribute("disabled", "disabled");
 		socket.emit("send message", messageInput.value, (message) => {
+			submitBtn.removeAttribute("disabled");
 			console.log(`${message}`);
 		});
 	}
 });
 submitBtn.addEventListener("click", (e) => {
+	submitBtn.setAttribute("disabled", "disabled");
 	e.preventDefault();
 	socket.emit("send message", `${messageInput.value}`, (message) => {
+		submitBtn.removeAttribute("disabled");
 		console.log(`${message}`);
 	});
 });
@@ -24,9 +37,12 @@ sendLocationBtn.addEventListener("click", (e) => {
 	e.preventDefault();
 	if (!navigator.geolocation) return alert("Can't find location!");
 
+	sendLocationBtn.setAttribute("disabled", "disabled");
+
 	navigator.geolocation.getCurrentPosition((position) => {
 		const { latitude, longitude } = position.coords;
 		socket.emit("send location", { latitude, longitude }, (message) => {
+			sendLocationBtn.removeAttribute("disabled");
 			console.log(`${message}`);
 		});
 	});
